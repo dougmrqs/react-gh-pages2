10efeebb27c3034312a2f7e215ca7fa76991ecb1
Douglas Marques <douglas.bmarques@hotmail.com>
2020-02-29 11:34:16 -0300

    Updates

---
+++ precache-manifest.a08e238e70ea5129de3fab980f1792b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c9c630ac013b4c5c859867199dd961",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "48ab6ec181a8f202aaaf",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4b664b83983b4b4aa951",
+    "url": "/react-gh-pages/static/js/2.8f174c8f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gh-pages/static/js/2.8f174c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ab6ec181a8f202aaaf",
+    "url": "/react-gh-pages/static/js/main.cff3fefb.chunk.js"
+  },
+  {
+    "revision": "54001be62eebd59a3712",
+    "url": "/react-gh-pages/static/js/runtime-main.8f268fec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
